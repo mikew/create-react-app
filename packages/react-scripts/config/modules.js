bc92abea7e71dc5973abec65ef06892d6a05129b
Mike Wyatt <wyatt.mike@gmail.com>
2019-12-17 03:08:11 -0400

    cleanup getModules

--- packages/react-scripts/config/modules.js
+++ packages/react-scripts/config/modules.js
@@ -108,40 +108,41 @@ function getJestAliases(options = {}) {
 
 function getModules() {
   // Check if TypeScript is setup
-  const hasTsConfig = fs.existsSync(paths.appTsConfig);
-  const hasJsConfig = fs.existsSync(paths.appJsConfig);
+  const hasTsConfig = true;
+  // const hasJsConfig = fs.existsSync(paths.appJsConfig);
 
-  if (hasTsConfig && hasJsConfig) {
-    throw new Error(
-      'You have both a tsconfig.json and a jsconfig.json. If you are using TypeScript please remove your jsconfig.json file.'
-    );
-  }
+  // if (hasTsConfig && hasJsConfig) {
+  //   throw new Error(
+  //     'You have both a tsconfig.json and a jsconfig.json. If you are using TypeScript please remove your jsconfig.json file.'
+  //   );
+  // }
 
   let config;
 
   // If there's a tsconfig.json we assume it's a
   // TypeScript project and set up the config
   // based on tsconfig.json
-  if (hasTsConfig) {
-    const ts = require(resolve.sync('typescript', {
-      basedir: paths.appNodeModules,
-    }));
-    config = ts.readConfigFile(paths.appTsConfig, ts.sys.readFile).config;
-    // Otherwise we'll check if there is jsconfig.json
-    // for non TS projects.
-  } else if (hasJsConfig) {
-    config = require(paths.appJsConfig);
-  }
+  // if (hasTsConfig) {
+  //   const ts = require(resolve.sync('typescript', {
+  //     basedir: paths.appNodeModules,
+  //   }));
+  //   config = ts.readConfigFile(paths.appTsConfig, ts.sys.readFile).config;
+  //   // Otherwise we'll check if there is jsconfig.json
+  //   // for non TS projects.
+  // } else if (hasJsConfig) {
+  //   config = require(paths.appJsConfig);
+  // }
 
-  config = config || {};
-  const options = config.compilerOptions || {};
+  // config = config || {};
+  // const options = config.compilerOptions || {};
+  config = readTypescriptConfig();
 
-  const additionalModulePaths = getAdditionalModulePaths(options);
+  // const additionalModulePaths = getAdditionalModulePaths(options);
 
   return {
-    additionalModulePaths: additionalModulePaths,
-    webpackAliases: getWebpackAliases(options),
-    jestAliases: getJestAliases(options),
+    additionalModulePaths: getAdditionalModulePaths(config.options),
+    webpackAliases: getWebpackAliases(config.options),
+    jestAliases: getJestAliases(config.options),
     hasTsConfig,
   };
 }

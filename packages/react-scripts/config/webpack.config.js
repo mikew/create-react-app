a2cb62648170657a02754167f002bef4a77f243f
Mike Wyatt <wyatt.mike@gmail.com>
2020-01-03 17:13:39 -0400

    just use the dang eslintrc

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -356,27 +356,15 @@ module.exports = function(webpackEnv) {
                 resolvePluginsRelativeTo: __dirname,
                 // @remove-on-eject-begin
                 ignore: process.env.EXTEND_ESLINT === 'true',
-                baseConfig: (() => {
-                  // We allow overriding the config only if the env variable is set
-                  if (process.env.EXTEND_ESLINT === 'true') {
-                    const eslintCli = new eslint.CLIEngine();
-                    let eslintConfig;
-                    try {
-                      eslintConfig = eslintCli.getConfigForFile(
-                        paths.appIndexJs
-                      );
-                    } catch (e) {
-                      console.error(e);
-                      process.exit(1);
-                    }
-                    return eslintConfig;
-                  } else {
-                    return {
-                      extends: [require.resolve('eslint-config-react-app-tsc')],
-                    };
-                  }
-                })(),
-                useEslintrc: false,
+                baseConfig:
+                  process.env.EXTEND_ESLINT === 'true'
+                    ? undefined
+                    : {
+                        extends: [
+                          require.resolve('eslint-config-react-app-tsc'),
+                        ],
+                      },
+                useEslintrc: process.env.EXTEND_ESLINT === 'true',
                 // @remove-on-eject-end
               },
               loader: require.resolve('eslint-loader'),

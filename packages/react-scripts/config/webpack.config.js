566a6e7526e3ec4f82385ed1907553e39b7a8cd2
Mike Wyatt <wyatt.mike@gmail.com>
2020-01-11 23:02:21 -0400

    don't cache eslint (again?)

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -350,7 +350,6 @@ module.exports = function(webpackEnv) {
           use: [
             {
               options: {
-                cache: true,
                 formatter: require.resolve('react-dev-utils/eslintFormatter'),
                 eslintPath: require.resolve('eslint'),
                 resolvePluginsRelativeTo: __dirname,
